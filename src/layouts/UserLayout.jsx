4cd91fe21948d055a91f53d16e5e7fc5f46876be
JackeBluthH <hdx001@sohu.com>
2021-01-24 21:52:20 +0800

    add favicon.ico

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -3,56 +3,31 @@
  */
 
 import React from 'react';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 import Header from '@/components/Header';
 import Sidebar from '@/components/Sidebar';
 import Footer from '@/components/Footer';
 
-// import styles from './index.less';
-// import { getMenuData, getPageTitle } from '@ant-design/pro-layout';
-// import { useIntl, connect } from 'umi';
-
 import './index.css';
 
 const UserLayout = (props) => {
-    // const {
-    //     route = {
-    //         routes: [],
-    //     },
-    // } = props;
-    // const { routes = [] } = route;
-    const { children, title="Image Mark" } = props;
-    // const { formatMessage } = useIntl();
-    // const { breadcrumb } = getMenuData(routes);
-    // const title = getPageTitle({
-    //     pathname: location.pathname,
-    //     formatMessage,
-    //     breadcrumb,
-    //     ...props,
-    // });
+    const { children } = props;
     return (
-        <HelmetProvider>
-            <Helmet>
-                <title>{title}</title>
-                <meta name="description" content={title} />
-            </Helmet>
-            <div className="app-layout">
-                <div className="layout-panel layout-header">
-                    <Header />
-                </div>
-                <div className="layout-panel layout-sidebar">
-                    <Sidebar />
-                </div>
-                <div className="layout-panel layout-content">
-                    {children}
-                    <Footer />
-                </div>
-                <div className="layout-panel layout-footer">
-                    
-                </div>
+        <div className="app-layout">
+            <div className="layout-panel layout-header">
+                <Header />
             </div>
-        </HelmetProvider>
+            <div className="layout-panel layout-sidebar">
+                <Sidebar />
+            </div>
+            <div className="layout-panel layout-content">
+                {children}
+                <Footer />
+            </div>
+            <div className="layout-panel layout-footer">
+
+            </div>
+        </div>
     );
 };
 
